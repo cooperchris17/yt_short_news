407db6de38d4e2531964809c4e0d7bdb37a1d6fc
cooperchris17 <87263328+cooperchris17@users.noreply.github.com>
2022-03-02 19:44:26 +0900

    Update C_combine_df.py

--- C_sampling/C_combine_df.py
+++ C_sampling/C_combine_df.py
@@ -4,20 +4,20 @@ This script combines the monthly metadata to check the monthly upload_month_dist
 
 import pandas as pd
 
-csv_out = 'Twelve_Channels_2021.csv'
+csv_out = 'sample_720_all_channels.csv'
 
-df1 = pd.read_csv('ABC_News_transcript_metadata.csv')
-df2 = pd.read_csv('Al_Jazeera_transcript_metadata.csv')
-df3 = pd.read_csv('Arirang_News_transcript_metadata.csv')
-df4 = pd.read_csv('BBC_News_transcript_metadata.csv')
+df1 = pd.read_csv('ABC_News_metadata_sample.csv')
+df2 = pd.read_csv('Al_Jazeera_metadata_sample.csv')
+df3 = pd.read_csv('Arirang_News_metadata_sample.csv')
+df4 = pd.read_csv('BBC_News_metadata_sample.csv')
 df5 = pd.read_csv('CBC_News_transcript_metadata.csv')
-df6 = pd.read_csv('CGTN_transcript_metadata.csv')
-df7 = pd.read_csv('CNA_transcript_metadata.csv')
+df6 = pd.read_csv('CGTN_metadata_sample.csv')
+df7 = pd.read_csv('CNA_metadata_sample.csv')
 df8 = pd.read_csv('DW_News_transcript_metadata.csv')
-df9 = pd.read_csv('i24_News_transcript_metadata.csv')
-df10 = pd.read_csv('Nippon_TV_transcript_metadata.csv')
-df11 = pd.read_csv('TVC_News_transcript_metadata.csv')
-df12 = pd.read_csv('WION_transcript_metadata.csv')
+df9 = pd.read_csv('i24_News_metadata_sample.csv')
+df10 = pd.read_csv('Nippon_TV_metadata_sample.csv')
+df11 = pd.read_csv('TVC_News_metadata_sample.csv')
+df12 = pd.read_csv('WION_metadata_sample.csv')
 
 frames = [df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12]
 
